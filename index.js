a511a26f13f76d04bb5049b8c789f46cda57d2ca
WASEEM AHMAD MEER <120891700+MohammadWaseem6@users.noreply.github.com>
2023-10-19 22:21:14 +0530

    http server

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+// Import required Node.js modules
+const http = require("http");
 const fs = require("fs");
-//syc........
-// fs.writeFileSync("./ waseem.txt", "hello waseem");
-//async.........
-// fs.writeFileSync("./ waseem.txt", "hello waseem async")
-// const result = fs.readFileSync("./ contact.txt", "utf-8");
-// console.log(result);
-fs.appendFileSync("./waseem.txt", new Date().getTime().toLocaleString);
+const url = require("url");
+
+// Create an HTTP server using the http module
+const myServer = http.createServer((req, res) => {
+    // Ignore requests for "/favicon.ico"
+    if (req.url === "/favicon.ico") {
+        res.end();
+        return;
+    }
+
+    // Generate a log entry for each incoming request
+    const log = `${Date.now()}: ${req.url} - New Request Received :\n`;
+    const MYUrl = url.parse(req.url);
+    console.log(MYUrl);
+    // Append the log entry to a log file
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) {
+            console.error("Error writing to log file:", err);
+        } else {
+            console.log("Log entry added to log.txt");
+        }
+
+        // Send a response to the client
+        res.end("hello from server Again");
+    });
+});
+
+// Start the server and make it listen on port 8000
+myServer.listen(8000, () => {
+    console.log("Server Started! Listening on port 8000.");
+});
